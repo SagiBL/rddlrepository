097afc5e0bd20fe92a9201b6621c1ce348a099e0
danno120 <danno120@gmail.com>
2024-11-03 14:05:45 +0200

    idk

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from rddlrepository.core.manager import RDDLRepoManager
 manager = RDDLRepoManager(rebuild=True)
 import agent1
+
+import pyRDDLGym
 import pyRDDLGym.core.policy
 
 env = pyRDDLGym.make('TrafficBLX_SimplePhases', 0)
@@ -14,7 +16,7 @@ state, _ = env.reset()
 for step in range(1,200):
     print("step",step)
     env.render(to_display=True)
-    action = agent.sample_action(state)
+    action = agent.sample_action(state,cmlt_reward)
     next_state, reward, terminated, truncated, _ = env.step(action)
     cmlt_reward = cmlt_reward + reward
     state = next_state
